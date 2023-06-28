c8b0c2358696827a8f28c00ba15c0fc1eefd1d7a
lauramorales <ma.lauramorales@gmail.com>
2023-06-28 12:55:38 -0300

    2

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,28 @@ import ItemListContainer from "./componentes/ItemListContainer/ItemListContainer
 import Categorias from "./componentes/Categorias/Categorias"
 import ItemDetailContainer from "./componentes/ItemDetailContainer/ItemDetailContainer"
 import Contador from "./componentes/Contador/Contador"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 
 
 const App = () => {
   return (
     <>
-      
+      <BrowserRouter>
+      <Routes>
+        <Route path="/"/>
+        <Route/>
+        <Route/>
+        <Route/>
+
+      </Routes>    
+      </BrowserRouter>
+
+
+
       <TituloPrincipal/> 
       <NavBar/> 
-      <ItemListContainer greeting = {"Beauty comes from the inside.Inside the hair salon. /n La Mala"}/>   
+      <ItemListContainer greeting = {"Beauty Comes From The Inside.Inside The Hair Salon.  La Mala"}/>   
       <Contador stock={10} inicial={1}/>
       <Categorias/>
       <ItemDetailContainer/>
