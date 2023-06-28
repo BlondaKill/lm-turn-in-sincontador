4478a7dc786dc3d0522b7e700da564fc2f5572c4
lauramorales <ma.lauramorales@gmail.com>
2023-06-28 15:54:23 -0300

    5

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,12 @@ import ItemListContainer from "./componentes/ItemListContainer/ItemListContainer
 import Categorias from "./componentes/Categorias/Categorias"
 import ItemDetailContainer from "./componentes/ItemDetailContainer/ItemDetailContainer"
 import Contador from "./componentes/Contador/Contador"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import CartWidget from "./componentes/CartWidget/CartWidget"
+import Item from "./componentes/Item/Item"
+import ItemDetail from "./componentes/ItemDetail/ItemDetail"
+import ItemList from "./componentes/ItemList/ItemList"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Navbar } from "react-bootstrap"
 
 
 
@@ -13,11 +18,16 @@ const App = () => {
     <>
       <BrowserRouter>
       <Routes>
-        <Route path="/"/>
-        <Route/>
-        <Route/>
-        <Route/>
-
+        <Route path="/CartWidget" element={<CartWidget/>}/>
+        <Route path="/Categorias" element={<Categorias/>}/>
+        <Route path="/Contador" element={<Contador/>}/>
+        <Route path="/Item" element={<Item/>}/>
+        <Route path="/ItemDetail" element={<ItemDetail/>}/>
+        <Route path="/ItemDetailContainer" element={<ItemDetailContainer/>}/>
+        <Route path="/ItemList" element={<ItemList/>}/>
+        <Route path="/ItemListContainer" element={<ItemListContainer/>}/>
+        <Route path="/Navbar" element={<Navbar/>}/>
+        <Route path="/TituloPrincipal" element={<TituloPrincipal/>}/>
       </Routes>    
       </BrowserRouter>
 
