d7d122f7c900916351244f6b7cb0c07a73e147ac
lauramorales <ma.lauramorales@gmail.com>
2023-07-03 14:51:00 -0300

    nav

--- src/componentes/NavBar/NavBar.jsx
+++ src/componentes/NavBar/NavBar.jsx
@@ -6,21 +6,21 @@ const NavBar = () => {
     return (
         <header>
             <Link to="/">
-                <h1>La Mala</h1>
+                <h1 className={"nav-link"}>La Mala</h1>
             </Link>
 
             <nav>
                 <ul>
                     <li>
-                        <NavLink to="/categoria/Blondes"> Blondes </NavLink>
+                        <NavLink className={"nav-link"} to="/categoria/Blondes"> Blondes </NavLink>
                     </li>
 
                     <li>
-                        <NavLink  to="/categoria/Brunette"> Brunette </NavLink>
+                        <NavLink  className={"nav-link"} to="/categoria/Brunette"> Brunette </NavLink>
                     </li>
 
                     <li>
-                        <NavLink  to="/categoria/Darks"> Darks </NavLink>
+                        <NavLink className={"nav-link"} to="/categoria/Darks"> Darks </NavLink>
                     </li>
                 </ul>
             </nav>
